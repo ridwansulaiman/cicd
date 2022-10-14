ff9fd0be7812222fedbca2af1e9dd07398ad0bdc
ridwansulaiman <87976706+ridwansulaiman@users.noreply.github.com>
2022-10-14 19:39:58 +0700

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, Hope you are doing well Ridwan !!');
+  res.send('Hello World, Hope you are doing well Ridwan, wanna coffe !!');
 });
 
 app.listen(PORT, HOST);
