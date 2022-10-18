e5de6c71fe77a36b15055ba19fc73f085c5e2d0a
ridwansulaiman <87976706+ridwansulaiman@users.noreply.github.com>
2022-10-18 11:31:18 +0700

    update apps

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, IAM NEW !!');
+  res.send('Hello World, this is new release !!');
 });
 
 app.listen(PORT, HOST);
