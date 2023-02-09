1aa209da8ba4472d8a18aa8cd2715f69413795e8
ridwansulaiman <87976706+ridwansulaiman@users.noreply.github.com>
2023-02-09 18:50:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, THIS DEMO APPS FOR PEGADAIAN !!');
+  res.send('HI THERE, THIS DEMO APPS FOR PEGADAIAN !!');
 });
 
 app.listen(PORT, HOST);
