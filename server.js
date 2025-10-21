c735860c2bdd297a56371d7627c640a96ccbbeef
ridwansulaiman <87976706+ridwansulaiman@users.noreply.github.com>
2025-10-21 09:48:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('HI APS TEAM!! WELCOME TO GOOGLE OFFICE!! 23 OCTOBER 2025');
+  res.send('HI APS TEAM!! WELCOME TO GOOGLE OFFICE!! 23 OCTOBER 2025 - PHASE 2');
 });
 
 app.listen(PORT, HOST);
